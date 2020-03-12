fc52e35ff072d6da31ac5654d81d43e6d14a6a46
Ojo Oluwasetemi <setemiojo@gmail.com>
2020-03-12 11:50:23 +0100

    📦 NEW: initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+const { ApolloServer, PubSub } = require('apollo-server-express');
+const express = require('express');
+const expressPlayground = require('graphql-playground-middleware-express').default;
+const { readFileSync } = require('fs');
+const { createServer } = require('http');
+
+
+const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8');
+const resolvers = require('./resolvers');
+
+
+async function start() {
+    // Create a new instance of the server
+    const app = express();
+
+    // Send it an object with typeDefs(the schema) and resolvers
+    const pubsub = new PubSub();
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+
+    server.applyMiddleware({ app });
+
+    app.get('/', (req, res) => {
+        res.end(`<a href="${url}">Welcome to the buycoins test</a>`)
+    });
+
+    app.get('/graphiql', expressPlayground({ endpoint: '/graphql' }));
+
+
+    const httpServer = createServer(app);
+    server.installSubscriptionHandlers(httpServer);
+
+    // Call listen on the server to launch the web server
+    httpServer.listen({ port: 4000 }, () =>
+        console.log(`GraphQL Server running at http://localhost:4000${server.graphqlPath} and socket is running at ws://localhost:4000/graphql`)
+    );
+
+
+}
+
+start();
---
+++ resolvers.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+
+const conversions = [{ id: 1, result: 340000 }];
+
+const Query = {
+  calculatePrice: async (parent, args) => {
+    // Get the percentage
+    const margin = args.margin / 100;
+    // Get bitcoin-dollar rate and make computations with supplied values
+    const { data } = await axios(
+      'https://api.coindesk.com/v1/bpi/currentprice.json'
+    );
+
+    const dollarRate = data.bpi.USD.rate_float;
+
+    let updatedRate = 0;
+
+    if (args.type === 'buy') {
+      updatedRate = dollarRate + dollarRate * margin;
+    } else if (args.type === 'sell') {
+      updatedRate = dollarRate - dollarRate * margin;
+    } else {
+      const defaultConversionResult = {
+        id: 0,
+        result: null
+      };
+      conversions.push(defaultConversionResult);
+
+      return defaultConversionResult;
+    }
+
+    /**
+     * Final conversion result is the product of the computed rate
+     * and the supplied exchange naira-dollar rate
+     */
+    const result = {
+      id: conversions.length + 1,
+      result: (updatedRate * args.exchangeRate).toFixed(2)
+    };
+
+    // Add the records of the conversion to our 'makeshift' database
+    conversions.push(result);
+    // Return output of conversion
+    return result;
+  },
+  allConversions: (parent, args) => conversions
+};
+
+const resolvers = {
+  Query
+};
+
+module.exports = resolvers;
